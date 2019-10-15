ced6e45fd52883eb06514c9b56902dc7e41cc186
Shivendra Singh Tanwar <sunnysinghtanwar2@gmail.com>
2019-10-15 12:16:25 +0530

    Update project list page

--- Projects/js/projects.js
+++ Projects/js/projects.js
@@ -16,16 +16,27 @@ require(['json!../projects.json'], function (projectJson) {
     for(var j=0;j<projectList.length;j++){
         var recordDiv = document.createElement("DIV");
 
-        let classesToAdd = [ 'flex', 'bg-white', 'min-h-full', 'rounded-lg', 'mt-5', 'mb-5', 'p-6','flex-col','shadow-lg' ];
+        let classesToAdd = [ 'flex', 'bg-white', 'min-h-full', 'rounded-sm', 'mt-5', 'mb-5', 'p-6','flex-col','shadow-lg' ];
         recordDiv.classList.add(...classesToAdd);
-        var recordData = document.createTextNode("Name : "+projectList[j].name);
+        
+        let projectTitleFormatting = ['font-bold','text-xl', 'mb-2'];
+        
+        //Add title 
+        var projectTitle = document.createElement("p");        
+        projectTitle.appendChild(document.createTextNode(projectList[j].name));
+        projectTitle.classList.add(...projectTitleFormatting);
 
-        recordDiv.appendChild(recordData);
-        recordDiv.appendChild(document.createElement("br"));
+        //Add Decription of project
+        var projectDesc = document.createElement("p");
+        projectDesc.appendChild(document.createTextNode(projectList[j].desc));
+
+
+        recordDiv.appendChild(projectTitle);
+        recordDiv.appendChild(projectDesc);
 
         var linkTag = document.createElement("a")
         linkTag.setAttribute('href',projectList[j].gitLink);
-        var link = document.createTextNode("Link To Project!");
+        var link = document.createTextNode("Go To Project ->");
         linkTag.appendChild(link)
         recordDiv.appendChild(linkTag)
         // console.log(recordDiv);
